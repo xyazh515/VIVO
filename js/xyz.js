647f22e214997d8366279dbe76903778f64e9b18
xyazh515 <619399498@qq.com>
2017-03-01 17:31:38 +0800

    111

--- js/xyz.js
+++ js/xyz.js
@@ -1,6 +1,6 @@
 			var lis = document.getElementById("vivo_ul1");
 			var obj_lis = lis.getElementsByTagName("li");
-			for(i=2;i<7;i++){
+			for(i=1;i<7;i++){
 				obj_lis[i].onmouseover = function(){
 					obj_lis[0].style.backgroundColor='#FFFFFF';
 					obj_lis[0].style.color = '#666666';
@@ -10,12 +10,22 @@
 					obj_lis[0].style.color = '#fff';
 				}  
 			}
-			obj_lis[1].onmouseover = function(){
+			document.getElementById("chanpin1").onmouseover = function(){
+					document.getElementById("chanpin").style.display="block";
+					obj_lis[0].style.backgroundColor='#FFFFFF';
+					obj_lis[0].style.color = '#666666';
+				}
+			document.getElementById("chanpin").onmouseover = function(){
 					document.getElementById("chanpin").style.display="block";
 					obj_lis[0].style.backgroundColor='#FFFFFF';
 					obj_lis[0].style.color = '#666666';
 				}
 				document.getElementById("chanpin").onmouseout = function(){
+					document.getElementById("chanpin").style.display="none";
+					obj_lis[0].style.backgroundColor='#008cd6';
+					obj_lis[0].style.color = '#fff';
+				}
+				document.getElementById("chanpin1").onmouseout = function(){
 					document.getElementById("chanpin").style.display="none";
 					obj_lis[0].style.backgroundColor='#008cd6';
 					obj_lis[0].style.color = '#fff';
